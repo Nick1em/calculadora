c67e94c0565a8c072f9999d5be41ade0848add7f
Nick1em <nicolekanandadeoc@gmail.com>
2025-05-08 20:49:28 -0300

    Primeira versão dos testes de calculadora

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+ const [input1,setInput1] = useState(0);
+ const [input2,setImput2] = useState(0);
+ const [operacao,setOperacao] = useState('+')
+ const [resultado,setResultado] = useState(0)
+
+ const calcular = () => {
+  if (operacao == "+")
+    return parseFloat(input1) + parseFloat(input2);
+  else if (operacao == "-")
+    return parseFloat(input1) - parseFloat(input2);
+  else if (operacao == "x")
+    return parseFloat(input1) * parseFloat(input2);  
+  else 
+    return parseFloat(input1) / parseFloat(input2)
+ }
+
+ useEffect (() => {
+  setResultado (calcular());
+ }, [input1,input2, operacao]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Calculadora</h1>
+      <input type='number'
+      onChange={(e) => setInput1(e.target.value)} 
+      value={input1} placeholder='Digite um número!'></input>
+
+      <select onChange={(e) => setOperacao(e.target.value)}>
+        <option>+</option>
+        <option>-</option>
+        <option>x</option>
+        <option>/</option>
+      </select>
+
+      <input type='number'
+      onChange={(e) => setImput2(e.target.value)} 
+      value={input2} placeholder='Digite um número!'></input>
+      
+      <label>Resultato {resultado}</label>
+      
     </div>
   );
 }
